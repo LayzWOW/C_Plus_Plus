7f35c08e70a006788ca441dc80313ade8e850b6e
Daryl <darylhb2006@gamil.com>
2026-03-20 16:11:00 +0800

    updates

--- projects/ToDo_manager/main.cpp
+++ projects/ToDo_manager/main.cpp
@@ -1,42 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
+#include <gtkmm.h>
+#include <cairomm/context.h>
 
-using namespace std;
+class MyArea : public Gtk::DrawingArea {
+protected:
+      // This is where the drawing happens
+      bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override {
+            Gtk::Allocation allocation = get_allocation();
+            const int width = allocation.get_width();
+            const int height = allocation.get_height();
 
-struct Task {
-      string description;
-      bool isDone;
-};
+            // Coordinates for the center
+            double xc = width / 2.0;
+            double yc = height / 2.0;
+            double radius = std::min(width, height) / 4.0;
 
-class TodoManager {
-private:
-      vector<Task> tasks;
-      const string filename = "tasks.txt";
+            // Draw circle
+            cr->set_line_width(5.0);
+            cr->set_source_rgb(0.7, 0.2, 0.2); // Reddish color
+            cr->arc(xc, yc, radius, 0.0, 2.0 * M_PI); // Full circle
+            cr->stroke();
 
-public:
-      void addTask(string desc) {
-            tasks.push_back({desc, false});
-      }
-
-      void showTasks() {
-            for (int i = 0; i < tasks.size(); i++) {
-                  cout << i + 1 << ". [" << (tasks[i].isDone ? "X" : " ") << "] " << tasks[i].description << endl;
-            }
-      }
-
-      void saveToFile() {
-            ofstream file(filename);
-            for (const auto& t : tasks) {
-                  file << t.description << "," << t.isDone << endl;
-            }
-            file.close();
+            return true;
       }
 };
 
-int main() {
-      TodoManager myTodo;
-      // Add your menu loop here
-      return 0;
+int main(int argc, char* argv[]) {
+      auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+
+      Gtk::Window win;
+      win.set_title("GTK Circle Bro");
+      win.set_default_size(400, 400);
+
+      MyArea area;
+      win.add(area);
+      area.show();
+
+      return app->run(win);
 }
